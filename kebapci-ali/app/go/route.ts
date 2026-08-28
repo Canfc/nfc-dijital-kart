917960d06842bd1e463be03fa8d9ffb335492364
Canfc <behlulcan41q@gmail.com>
2026-08-28 01:29:13 +0000

    Supabase publishable key duzeltildi

--- kebapci-ali/app/go/route.ts
+++ kebapci-ali/app/go/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 export async function GET() {
   const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.SUPABASE_PUBLISHABLE_KEY!
   );
 
   const { error } = await supabase
