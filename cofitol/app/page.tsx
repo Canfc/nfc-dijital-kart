34c4de46f8c7a336c98ad4f7040922ed6bc70511
Canfc <behlulcan41q@gmail.com>
2026-08-31 22:46:31 +0000

    Coffitol iPhone tam ekran duzeltildi

--- cofitol/app/page.tsx
+++ cofitol/app/page.tsx
@@ -1,42 +1,94 @@
 export default function Home() {
   return (
-    <main className="min-h-[100svh] bg-[#2b220d]">
-      <div className="relative mx-auto h-[100svh] w-full max-w-[430px] overflow-hidden">
-
+    <main
+      style={{
+        position: "fixed",
+        inset: 0,
+        width: "100%",
+        height: "100dvh",
+        overflow: "hidden",
+        backgroundColor: "#2b220d",
+      }}
+    >
+      <div
+        style={{
+          position: "relative",
+          width: "100%",
+          height: "100%",
+          maxWidth: "430px",
+          margin: "0 auto",
+          overflow: "hidden",
+          backgroundColor: "#2b220d",
+        }}
+      >
         <img
           src="/cofitol-panel.png"
           alt="Coffitol Coffee House"
-          className="absolute inset-0 h-full w-full object-cover object-top"
+          style={{
+            position: "absolute",
+            inset: 0,
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+            objectPosition: "center center",
+          }}
         />
 
         {/* GOOGLE */}
         <a
           href="/click?link=google"
           aria-label="Google Yorum"
-          className="absolute left-[5%] top-[39.5%] z-20 h-[22%] w-[22%]"
+          style={{
+            position: "absolute",
+            left: "5%",
+            top: "39.5%",
+            width: "22%",
+            height: "22%",
+            zIndex: 20,
+          }}
         />
 
         {/* INSTAGRAM */}
         <a
           href="/click?link=instagram"
           aria-label="Instagram"
-          className="absolute left-[28.5%] top-[39.5%] z-20 h-[22%] w-[22%]"
+          style={{
+            position: "absolute",
+            left: "28.5%",
+            top: "39.5%",
+            width: "22%",
+            height: "22%",
+            zIndex: 20,
+          }}
         />
 
         {/* KONUM */}
         <a
           href="/click?link=konum"
           aria-label="Konum"
-          className="absolute left-[52%] top-[39.5%] z-20 h-[22%] w-[22%]"
+          style={{
+            position: "absolute",
+            left: "52%",
+            top: "39.5%",
+            width: "22%",
+            height: "22%",
+            zIndex: 20,
+          }}
         />
 
         {/* TELEFON */}
         <a
           href="/click?link=telefon"
           aria-label="Telefon"
-          className="absolute left-[75.5%] top-[39.5%] z-20 h-[22%] w-[19%]"
+          style={{
+            position: "absolute",
+            left: "75.5%",
+            top: "39.5%",
+            width: "19%",
+            height: "22%",
+            zIndex: 20,
+          }}
         />
-
       </div>
     </main>
   );
